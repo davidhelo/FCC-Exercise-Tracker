e3805b8c80f963d05f70e21bf626d4d547cbdf01
dahelo <dohernandez93@gmail.com>
2023-03-02 17:34:00 +0000

    feat: exercise mongoose schema and model added

--- mongooseDB_connection.js
+++ mongooseDB_connection.js
@@ -7,6 +7,15 @@ let userSchema = new mongoose.Schema({
   username: String
 });
 
+let exerciseSchema = new mongoose.Schema({
+  username: String,
+  description: String,
+  duration: Number,
+  date: Date
+});
+
 let userModel = mongoose.model('userModel', userSchema);
+let exerciseModel = mongoose.model('exerciseModel', exerciseSchema);
 
 exports.userModel = userModel;
+exports.exerciseModel = exerciseModel;
