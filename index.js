5a48948c681ce0f520392a297b0960e221914c9d
dahelo <dohernandez93@gmail.com>
2023-03-08 22:26:37 +0000

    feat: api/users/exercise POST handler added. It can create new exercise and responds with registered exercise data

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { userModel } = require('./mongooseDB_connection.js')
+const { userModel, exerciseModel } = require('./mongooseDB_connection.js')
 const bodyParser = require('body-parser')
-
 
 app.use(cors())
 app.use(express.static('public'))
@@ -12,9 +11,9 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-// create new USER form > POST /api/users
 app.use('/api/users', bodyParser.urlencoded({extended: false}));
 
+// create new USER form > POST /api/users
 function newUserHandler (req, res) {
   let newUser = new userModel({
     username: req.body.username
@@ -50,7 +49,66 @@ function retrieveUsersHandler (req, res){
 app.get('/api/users', retrieveUsersHandler);
 
 // add EXERCISE form > POST /api/users/:_id/exercises
+app.use('/api/users/:_id/exercises', bodyParser.urlencoded({extended: false}));
 
+function exercisesHandler(req, res) {
+  //DELETE ALL EXERCISES -----------------------------------------
+  /*exerciseModel.deleteMany({})
+    .then((dataDelete) => {console.log(`All exercises deleted: ${dataDelete}`)})
+    .catch((errDelete) => {console.log(errDelete)});*/
+  //-------------------------------------------------------------
+  
+  // get username, find by id
+  userModel.findById({ _id: req.body[':_id'] })
+    .then((dataUser) => {
+      console.log(dataUser);
+      if (dataUser == null) {
+        console.log("user id not found");
+      } else {
+        // set current date if a date not provided
+        console.log(`date: ${req.body.date}`);
+        let dateToRegister = undefined;
+        if (req.body.date == "") {
+          dateToRegister = new Date(Date.now());
+        } else {
+          dateToRegister = new Date(req.body.date);
+        }
+
+        //Create new exercise model instance to save
+        let newExercise = new exerciseModel({
+          username: dataUser.username,
+          description: req.body.description,
+          duration: parseInt(req.body.duration),
+          date: dateToRegister.toDateString()
+        });
+
+        // save document
+        newExercise.save()
+          .then((dataExercise) => {
+            let exerciseRegistered = {
+              username: dataExercise.username,
+              description: dataExercise.description,
+              duration: dataExercise.duration,
+              date: dataExercise.date.toDateString().split('T')[0],
+              "_id": dataExercise._id
+            };
+            console.log("Exercise registered with data:");
+            console.log(exerciseRegistered);
+            res.json(exerciseRegistered);
+          })
+          .catch((errExercise) => {
+            console.log(errExercise);
+            res.send(errExercise);
+          })
+        }
+    })
+    .catch((error) => {
+        console.log(error);
+        res.send(error);
+    }); 
+};
+
+app.post('/api/users/:_id/exercises', exercisesHandler);
 
 // API > GET /api/users/:_id/logs?[from][&to][&limit]
 
