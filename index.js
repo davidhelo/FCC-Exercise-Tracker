102c8bbb1abda1f6bf8f45528ca21e6488ba118a
dahelo <dohernandez93@gmail.com>
2023-03-15 22:03:07 +0000

    feat: GET request addeed: API > GET /api/users/:_id/logs?[from][&to][&limit]

--- index.js
+++ index.js
@@ -112,17 +112,21 @@ function exercisesHandler(req, res) {
 app.post('/api/users/:_id/exercises', exercisesHandler);
 // **************************************************
 // api/users/:_id/logs
-
+// API > GET /api/users/:_id/logs?[from][&to][&limit] (Optional parameters)
 function logsHandler(req, res){
   console.log(`Id requested: ${req.params._id}`);
-  console.log(req.query.from);
-  console.log(req.query.to);
+  console.log(`Parameters for filters:`);
+  console.log(`From date: ${req.query.from}`);
+  console.log(`To date: ${req.query.to}`);
+  console.log(`limit: ${req.query.limit}`);
   let userId = req.params._id;
   userModel.findById({ "_id": userId })
   .select('username')
   .then((dataLogs) => {
     // if no document found it returns null
-    if (dataLogs == null) { res.send("user not found"); }
+    if (dataLogs == null) { 
+      res.end("<b>user not found</b>"); 
+    } else {
     console.log("Username found: ");
     console.log(dataLogs);
     // query to count documents with the user name found
@@ -134,12 +138,23 @@ function logsHandler(req, res){
         console.log(`Dcouments counted: ${documentsCount}`);
         
         // query to get all the logs with user name found
-        exerciseModel.find({ username: dataLogs.username })
-          //.where('date').gte(req.query.from).lte(req.query.to)
-          .select('-_id description duration date')
-          //.limit(parseInt(req.query.limit))
+        let queryExercise = exerciseModel.find({ username: dataLogs.username });
+        if (req.query.from != undefined) {
+          queryExercise.where('date').gte(req.query.from);
+        } 
+        if (req.query.to != undefined) {
+          queryExercise.where('date').lte(req.query.to);
+        } 
+        if (req.query.limit != undefined) {
+          queryExercise.limit(parseInt(req.query.limit));
+        }
+        queryExercise.select('-_id description duration date');
+        console.log("Filters applied to query:");
+        console.log(queryExercise.getFilter());
+          queryExercise.exec()
+          
           .then((dataLogsExercises) => {
-            console.log(`Exercises found for id: ${userId}`);
+            console.log(`Exercise records found for id: ${userId}`);
             let logResponse = {
                 username: dataLogs.username,
                 count: documentsCount,
@@ -156,22 +171,22 @@ function logsHandler(req, res){
             res.json(logResponse);
           })
           .catch((errLogsExercises) => {
-            console.log(errLogsExercises);
-            res.send(errLogsExercises);
+            console.log(errLogsExercises.message);
+            res.json(errLogsExercises.message);
           });
       })
-      .catch((errCount) => { console.log(errCount) });
+      .catch((errCount) => { console.log(errCount.message) });
+    }
   })
   .catch((errLogs) => {
-    console.log(errLogs);
-    res.send(errLogs);
+    console.log(errLogs.message);
+    res.send(errLogs.message);
   });
 };
 
 app.get('/api/users/:_id/logs', logsHandler);
 
-// API > GET /api/users/:_id/logs?[from][&to][&limit]
-
+// set listener
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
